cd715132255f58ffa793af584f97ce34f4b50449
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2025-11-25 19:12:27 +0300

    update!

--- src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
@@ -12,31 +12,34 @@ import {useState} from "react";
 
 export const PlaylistsPage = () => {
     const {data} = useGetPlaylistsQuery()
-    const [playlistId,setplaylistId]=useState<string | null>(null)
+    const [playlistId, setplaylistId] = useState<string | null>(null)
     const [deletePlayList] = useDeletePlaylistsMutation()
     const [UpdatePlaylist] = useUpdatePlaylistsMutation()
-    const { register, handleSubmit, reset } = useForm<CreatePlaylistArgs>()
+    const {register, handleSubmit, reset} = useForm<CreatePlaylistArgs>()
 
     const deletePlayListHandler = (playlistId: string) => {
         if (confirm('Are you sure you want to delete the playlist?')) {
             deletePlayList(playlistId)
         }
     }
-    const UpdatePlaylistHandler = (playlistId:string) => {
-        UpdatePlaylist({playlistId,
-            body:{
-                title: '3453',
-                description: '2',
+    const StartUpdatePlaylistHandler = (playlistId: string) => {
+        setplaylistId(playlistId)
+    }
+    const CanceleditPlaylistHandler = (arg: null) => {
+        setplaylistId(null)
+    }
+    const onSubmit: SubmitHandler<CreatePlaylistArgs> = (data) => {
+        UpdatePlaylist({
+            playlistId,
+            body: {
+                title: data.title,
+                description: data.description,
                 tagIds: []
             }
+        }).unwrap().finally(()=>{
+            setplaylistId(null)
         })
-    }
-const editPlaylistHandler=(arg:null)=>{
-    console.log(arg)
-}
-const onSubmit:SubmitHandler<CreatePlaylistArgs>=(data)=>{
-    console.log(data)
-}
+            }
     return (
         <div className={s.container}>
             <h1 className={s.title}>PlaylistsPage</h1>
@@ -44,33 +47,33 @@ const onSubmit:SubmitHandler<CreatePlaylistArgs>=(data)=>{
             <div className={s.items}>
                 {
                     data?.data.map((playlist) => {
-                        const isEditPlayList=playlistId===playlist.id
+                        const isEditPlayList = playlistId === playlist.id
                         return (
                             <div key={playlist.id} className={s.item}>
                                 {isEditPlayList ? (
-                                <form onSubmit={handleSubmit(onSubmit)}>
-                                    <h2>Editr playlist</h2>
-                                    <div>
-                                        <input {...register('title')} placeholder={'title'} />
-                                    </div>
-                                    <div>
-                                        <input {...register('description')} placeholder={'description'} />
-                                    </div>
-                                    <button type={'submit'}>save</button>
-                                    <button type={'button'} onClick={() => editPlaylistHandler(null)}>
-                                        cancel
-                                    </button>
-                                </form>)
-                                :
+                                        <form onSubmit={handleSubmit(onSubmit)}>
+                                            <h2>Editr playlist</h2>
+                                            <div>
+                                                <input {...register('title')} placeholder={'title'}/>
+                                            </div>
+                                            <div>
+                                                <input {...register('description')} placeholder={'description'}/>
+                                            </div>
+                                            <button type={'submit'}>save</button>
+                                            <button type={'button'} onClick={() => CanceleditPlaylistHandler(null)}>
+                                                cancel
+                                            </button>
+                                        </form>)
+                                    :
                                     (<div>
-                                <div> {playlist.attributes.title}</div>
-                                <div>{playlist.attributes.description}</div>
-                                <div>{playlist.attributes.user.name}</div>
-                                <button onClick={() => deletePlayListHandler(playlist.id)}>delete</button>
-                                <button onClick={() => UpdatePlaylistHandler(playlist.id)}>update</button>
-                                </div>
-                                )}
-                                </div>)
+                                            <div> {playlist.attributes.title}</div>
+                                            <div>{playlist.attributes.description}</div>
+                                            <div>{playlist.attributes.user.name}</div>
+                                            <button onClick={() => deletePlayListHandler(playlist.id)}>delete</button>
+                                            <button onClick={() => StartUpdatePlaylistHandler(playlist.id)}>update</button>
+                                        </div>
+                                    )}
+                            </div>)
                     })
                 }
             </div>
