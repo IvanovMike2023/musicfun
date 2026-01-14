f1b7de3a4bb83e60ab8f24d9005f2a17bd647611
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-01-14 12:00:59 +0300

    infiniteQueryOptions offset s

--- src/features/tracks/ui/TracksPage.tsx
+++ src/features/tracks/ui/TracksPage.tsx
@@ -4,7 +4,7 @@ import {useInfiniteScroll} from "@/common/hooks/useInfinityScroll";
 import {TracksList} from "@/features/tracks/ui/TracksList";
 
 export const TracksPage = () => {
-    const {data, isLoading, isFetching, hasNextPage, isFetchingNextPage, fetchNextPage} = useFetchTracksInfiniteQuery()
+    const {data, isFetching, hasNextPage, isFetchingNextPage, fetchNextPage} = useFetchTracksInfiniteQuery()
     const pages = data?.pages.flatMap(page => page.data) || []
     const {observerRef} = useInfiniteScroll({isFetching, hasNextPage, fetchNextPage})
     return (
